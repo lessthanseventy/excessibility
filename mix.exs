6a03e1a089222cf7d699bed4464332e1e860fbff
Andrew Moore <andrew@lessthanseventy.com>
2023-04-04 13:13:09 -0600

    bump version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Excessibility.MixProject do
   def project do
     [
       app: :excessibility,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
