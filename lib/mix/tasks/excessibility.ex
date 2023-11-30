2dc4ad7dbf636dab492dd26aab87dd6da7a7ec4b
Andrew Moore <andrew@lessthanseventy.com>
2023-11-30 00:34:09 -0500

    add max_failures and max_percentage_failures config

--- lib/mix/tasks/excessibility.ex
+++ lib/mix/tasks/excessibility.ex
@@ -17,6 +17,17 @@ defmodule Mix.Tasks.Excessibility do
                )
   @snapshots_path "#{@output_path}/html_snapshots"
   @ex_assets_path "#{@output_path}/assets"
+  @max_failures Application.compile_env(
+                  :excessibility,
+                  :max_failures,
+                  0
+                )
+
+  @max_percentage_failures Application.compile_env(
+                             :excessibility,
+                             :max_percentage_failures,
+                             0
+                           )
 
   @impl Mix.Task
   def run(_args) do
@@ -26,12 +37,16 @@ defmodule Mix.Tasks.Excessibility do
     File.mkdir_p("#{@ex_assets_path}/js/")
     File.mkdir_p("#{@snapshots_path}/")
 
+    spinner_pid = spawn_link(fn -> spinner() end)
+
     @snapshots_path
     |> File.ls!()
     |> filter_dirs()
     |> run_pa11y()
     |> print_results()
-    |> exit_status()
+    |> exit_status(@max_failures, @max_percentage_failures)
+
+    Process.exit(spinner_pid, :normal)
   end
 
   defp filter_dirs(list_of_files) do
@@ -51,10 +66,13 @@ defmodule Mix.Tasks.Excessibility do
     end)
   end
 
-  defp exit_status(results) do
-    results
-    |> Enum.all?(fn {_, status} -> status == 0 end)
-    |> if do
+  defp exit_status(results, max_failures, max_percentage_failures) do
+    total_tests = length(results)
+    failures = Enum.filter(results, fn {_, status} -> status != 0 end)
+    num_failures = length(failures)
+    failure_percentage = num_failures / total_tests * 100
+
+    if num_failures <= max_failures || failure_percentage <= max_percentage_failures do
       System.stop(0)
     else
       if Mix.env() !== :test do
@@ -66,5 +84,35 @@ defmodule Mix.Tasks.Excessibility do
   defp print_results(results) do
     Enum.each(results, fn {result, _status} -> IO.puts(result) end)
     results
+  end
+
+  defp spinner do
+    spinner_chars = ["|", "/", "-", "\\"]
+    spinner_index = 0
+    IO.write("Testing for a11y violations, please wait:")
+    IO.puts("\e[B")
+    loop(spinner_chars, spinner_index)
+  end
+
+  defp loop(spinner_chars, spinner_index) do
+    IO.ANSI.clear_line()
+    IO.write("#{Enum.at(spinner_chars, spinner_index)}")
+    # Adjust the sleep duration as needed
+    :timer.sleep(300)
+    clear_line()
+
+    next_index =
+      if spinner_index == 3 do
+        0
+      else
+        spinner_index + 1
+      end
+
+    loop(spinner_chars, next_index)
+  end
+
+  defp clear_line do
+    IO.write("\e[2K")
+    IO.write("\r")
   end
 end
