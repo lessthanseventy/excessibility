67b83a5740d8cc066b24709595edacf4b7ac2592
Andrew Moore <andrew@lessthanseventy.com>
2025-12-11 18:56:16 -0700

    Installer enforces igniter dependency

--- lib/mix/tasks/install.ex
+++ lib/mix/tasks/install.ex
@@ -42,6 +42,7 @@ defmodule Mix.Tasks.Excessibility.Install do
 
     igniter
     |> ensure_test_helper_config(test_helper, endpoint)
+    |> ensure_igniter_dependency()
     |> ensure_floki_dependency()
     |> maybe_install_pa11y(assets_dir, skip_npm?)
   end
@@ -90,6 +91,10 @@ defmodule Mix.Tasks.Excessibility.Install do
 
   defp ensure_floki_dependency(igniter) do
     Deps.add_dep(igniter, {:floki, "~> 0.28"}, on_exists: :overwrite, yes?: true)
+  end
+
+  defp ensure_igniter_dependency(igniter) do
+    Deps.add_dep(igniter, {:igniter, "~> 0.7", runtime: false}, on_exists: :overwrite, yes?: true)
   end
 
   defp maybe_install_pa11y(igniter, assets_dir, true), do: add_npm_notice(igniter, assets_dir)
