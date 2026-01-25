ddbfb025ec8d04b59cb66a97c50d9699a66d0e4b
Andrew Moore <andrew@lessthanseventy.com>
2026-01-25 04:12:39 -0700

    Require Elixir ~> 1.14 for Ecto dependency compatibility

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Excessibility.MixProject do
     [
       app: :excessibility,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: false,
       name: "Excessibility",
       description: description(),
