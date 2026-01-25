a287a2c4988c85c2fbb24d0c56877f17d298b552
Andrew Moore <andrew@lessthanseventy.com>
2026-01-25 05:18:30 -0700

    Bump version to 0.8.5

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Excessibility.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/lessthanseventy/excessibility"
-  @version "0.8.4"
+  @version "0.8.5"
 
   def project do
     [
